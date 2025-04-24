eb8998165fab31467e84494a4970668e42f1041f
thalyssonmota <motathalysson@outlook.com>
2025-04-24 09:30:35 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
     <div className="w-full h-screen flex bg-[#201b2c]">
       <div className="w-[50%] h-full flex items-center justify-center flex-col">
         <h1 className="text-[#00ff88] text-[30px] font-bold">Entre para o nosso time</h1>
-        <img src={imgAnimada} alt="Imagem de Animação" width={500}/>
+        <img src={imgAnimada} alt="Imagem de Animação" width={500} />
 
       </div>
 
@@ -19,14 +19,17 @@ export default function App() {
             <input type="text" id="user" className="w-full rounded-md p-[10px] bg-[#514768] text-white mb-" />
             <label htmlFor="password" className="text-[#00ff88]">Senha</label>
             <input type="password" id="password" className="w-full rounded-md p-[10px] bg-[#514768] text-white" />
+            <div className="w-full flex items-center justify-center text-[#00ff88] pt-1">
+              <a href="">Esqueceu sua senha?</a>
+            </div>
           </div>
 
-          <div className="w-full h-[20%] py-[10px] px-[20px]">
-            <button className="w-full h-full bg-[#00ff88] font-bold rounded-md cursor-pointer">Entrar</button>
-            
+          <div className="w-full h-[20%] flex  justify-center items-center  p-[20px]">
+            <button className="w-full h-[50px] bg-[#00ff88] text-[18px] font-bold text-[#211B2C] rounded-md cursor-pointer shadow-lg shadow-[#33f79c63]">
+              Entrar
+            </button>
           </div>
         </div>
-
       </div>
     </div>
   )
